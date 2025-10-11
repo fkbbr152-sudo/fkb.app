73c1c771039e03a8c280a87829e6dbbdd2d24c68
fkbbr152-sudo <fkbbr152@gmail.com>
2025-10-10 21:19:19 -0300

    correção

--- src/app/model/environments/environments.ts
+++ src/app/model/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://fkb.br/fkb.app/back-login-fkb',
+  apiBaseUrl: 'https://fkb.br/fkb.app/back-login-fkb/',
   popupBaseUrl: 'https://fkb.br/fkb.app/popupLigaDesliga',
 };
